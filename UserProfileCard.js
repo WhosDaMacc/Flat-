dcadc35966d9f3eee3af310464c1581f8d8beb71
WhosDaMacc <142145705+WhosDaMacc@users.noreply.github.com>
2025-02-02 12:40:25 -0800

    Create UserProfileCard.js

---
+++ UserProfileCard.js
@@ -0,0 +1,9 @@
+// Bad: Unordered imports
+import React from 'react';
+import Button from './Button';
+import axios from 'axios';
+
+// Good: Ordered imports
+import React from 'react';
+import axios from 'axios';
+import Button from './Button';
